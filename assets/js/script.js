1819d2180aeb09e422e29bb1eb738a8ed6c22cb7
Simon Baev <simonbaev@gmail.com>
2015-11-28 20:57:18 -0500

    Intro text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
 	console.log("Welcome!")
+	$('.tab-content').swiperight(function() {
+		$('.nav-tabs > .active').prev('li').find('a').trigger('click');
+		console.log("right")
+	});
+	$('.tab-content').swipeleft(function() {
+		$('.nav-tabs > .active').next('li').find('a').trigger('click');
+		console.log("left")
+	});
+	$('#intro a[href=#setup]').click(function(){
+		$('.nav-tabs li a[href=#setup]').trigger('click');
+		return false;
+	});
+	$('#intro a[href=#play]').click(function(){
+		$('.nav-tabs li a[href=#play]').trigger('click');
+		return false;
+	});
 })
